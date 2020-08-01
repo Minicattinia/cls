a9575a1cddd54bf28ba311c665f820d2369571c9
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2020-08-01 15:19:16 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64;Linux; Android 7.1.1; CPH1801) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36";
+
+$cookie = "__cfduid=db506b81cf58cbac3b2aa1420136cd1741596268279; PHPSESSID=bosccichs8qq6b9u1nsr44nofr; _ga=GA1.2.1082031531.1596268279; _gid=GA1.2.446584953.1596268279; log_user_code=836151699; log_user_i=58592; data_1=58592";
+
+$userIdentity = "userIdentity=58592&userIdentLong=836151699&_=1596269167646";
+
+
+
+
